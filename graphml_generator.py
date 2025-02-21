0b06141d0487de8757e93dfd5217b74bd4367ee6
FranUrsus <francisco.rdg.gmz@gmail.com>
2025-02-21 13:35:09 +0100

    Creando nuevo repo

--- graphml_generator.py
+++ graphml_generator.py
@@ -2,9 +2,12 @@ import osmnx as ox
 
 # Define el área del Soho Málaga
 place_name = "Soho, Málaga, España"
+place_name = "Ensanche Centro, Málaga, España"
+nombre = 'soho_malaga'
+nombre = 'ensanche_malaga'
 
 # Descarga el grafo de la red de calles
 G = ox.graph_from_place(place_name, network_type="drive")
 
 # Guarda el grafo en formato GraphML
-ox.save_graphml(G, "soho_malaga.graphml")
+ox.save_graphml(G, f"{nombre}.graphml")
